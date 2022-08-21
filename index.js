f5d0c9fdd8d505c5a630051222338a4d6475dd50
Felix <flexxmosh@gmail.com>
2022-08-21 23:16:56 +0300

    commit tree 8f574a42ad300f91bc5dd6743b96549c389efb97

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = "bob";  //bob
+const leastCustomer = "ray";  //ray
+
+function upperCaseCustomerName(){
+    window.customerName = customerName.toUpperCase()
+    return window.customerName;
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+}
+
+function changeLeastFavoriteCustomer(){
+    leastCustomer = 'Luther'
+    return leastCustomer
+}
